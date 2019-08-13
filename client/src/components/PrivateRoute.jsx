655029c4c3569a2127c4ee684af1d50ffd6d6838
darpham <phamdarren1@gmail.com>
2019-08-12 20:25:44 -0700

    test

--- client/src/components/PrivateRoute.jsx
+++ client/src/components/PrivateRoute.jsx
@@ -3,37 +3,66 @@ import PropTypes from "prop-types";
 import { Route } from "react-router-dom";
 import { useAuth0 } from "../react-auth0-spa";
 
+
+// this original code block was from Auth0 GH, kept prompting for login again
+// https://github.com/auth0-samples/auth0-react-samples/blob/master/01-Login/src/components/PrivateRoute.js
+// 
+
+
+// const PrivateRoute = ({ component: Component, path, ...rest }) => {
+//   const { isAuthenticated, loginWithRedirect } = useAuth0();
+
+//   useEffect(() => {
+//     const fn = async () => {
+//       if (!isAuthenticated) {
+//         await loginWithRedirect({
+//           appState: { targetUrl: path },
+//           access_type: 'offline', 
+//           // Reminder: may need to add more
+//           // connection_scope: 'https://www.googleapis.com/auth/calendar.events.readonly', 
+//           // approval_prompt: 'force'
+//         });
+//       }
+//     };
+//     fn();
+//   }, [isAuthenticated, loginWithRedirect, path]);
+
+//   const render = props =>
+//     isAuthenticated === true ? <Component {...props} /> : null;
+
+//   return <Route path={path} render={render} {...rest} />;
+// };
+
+// PrivateRoute.propTypes = {
+//   component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+//     .isRequired,
+//   path: PropTypes.oneOfType([
+//     PropTypes.string,
+//     PropTypes.arrayOf(PropTypes.string)
+//   ]).isRequired
+// };
+
+
+// Used code block directly from https://auth0.com/docs/quickstart/spa/react/01-login
+// this help with no longer prompting login over and over again...
 const PrivateRoute = ({ component: Component, path, ...rest }) => {
-  const { isAuthenticated, loginWithRedirect } = useAuth0();
+  const { loading, isAuthenticated, loginWithRedirect } = useAuth0();
 
   useEffect(() => {
+    if (loading || isAuthenticated) {
+      return;
+    }
     const fn = async () => {
-      if (!isAuthenticated) {
-        await loginWithRedirect({
-          appState: { targetUrl: path },
-          access_type: 'offline', 
-          // Reminder: may need to add more
-          // connection_scope: 'https://www.googleapis.com/auth/calendar.events.readonly', 
-          // approval_prompt: 'force'
-        });
-      }
+      await loginWithRedirect({
+        appState: { targetUrl: path }
+      });
     };
     fn();
-  }, [isAuthenticated, loginWithRedirect, path]);
+  }, [loading, isAuthenticated, loginWithRedirect, path]);
 
-  const render = props =>
-    isAuthenticated === true ? <Component {...props} /> : null;
+  const render = props => isAuthenticated === true ? <Component {...props} /> : null;
 
   return <Route path={path} render={render} {...rest} />;
-};
-
-PrivateRoute.propTypes = {
-  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
-    .isRequired,
-  path: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.arrayOf(PropTypes.string)
-  ]).isRequired
 };
 
 export default PrivateRoute;
