fce2175723df44de7d978ecb4cbf2cfa5d5857ea
Fabien Mansoubi <fabienmp@gmail.com>
2019-08-13 15:34:48 -0700

    Cleanup App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
+
+// GENERIC REACT LIBS
 import React from "react";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Container } from "reactstrap";
 
+// AUTH ROUTE
 import PrivateRoute from "./components/PrivateRoute";
-import Loading from "./components/Loading";
+// COMPONENTS
 import NavBar from "./components/NavBar/NavBar.jsx";
 import Footer from "./components/Footer";
-import Home from "./views/Home";
+import Loading from "./components/Loading";
+// REQUIRE AUTH
 import Dashboard from "./views/Dashboard";
 import Profile from "./views/Profile";
+import ExternalApi from "./components/manual_triggers/ExternalApi";
+// DOES NOT REQUIRE AUTH
+import Home from "./views/Home";
 import Privacy from "./views/Privacy";
 import AgendaSubmission from "./views/AgendaSubmission";
 import LandingPage from "./views/LandingPage/LandingPage";
+// LIB
 import { useAuth0 } from "./react-auth0-spa";
-import ExternalApi from "./components/manual_triggers/ExternalApi";
-
-// styles
+// APP STYLES 
 import "./App.css";
 
-// fontawesome
+// FONTS
 import initFontAwesome from "./utils/initFontAwesome";
 initFontAwesome();
 
@@ -37,18 +43,16 @@ const App = () => {
     <BrowserRouter>
       <div id="app" className="d-flex flex-column h-100">
         <NavBar />
-        <div style={style}>
-        {/* <Container className="flex-grow-1 mt-5"> */}
+        <div style={style}>     
           <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/landing" exact component={LandingPage} />
+            <Route path="/privacy" exact component={Privacy} />
+            <Route path="/agenda-submission" exact component={AgendaSubmission} />
             <PrivateRoute path="/dashboard" component={Dashboard} />
             <PrivateRoute path="/profile" component={Profile} />
             <PrivateRoute path="/external-api" component={ExternalApi} />
-            <Route path="/privacy" exact component={Privacy} />
-            <Route path="/agenda-submission" exact component={AgendaSubmission} />
           </Switch>
-        {/* </Container> */}
         </div>
         <Footer />
       </div>
