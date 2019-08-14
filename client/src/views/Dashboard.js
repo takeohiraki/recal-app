2b443325c3efec80513570831756c7c35c243ccc
Fabien Mansoubi <fabienmp@gmail.com>
2019-08-13 17:47:38 -0700

    Now loading data on load for dashboard

--- client/src/views/Dashboard.js
+++ client/src/views/Dashboard.js
@@ -1,11 +1,52 @@
-import React, { Fragment } from "react";
+import React, {
+  Fragment,
+  useState,
+  useEffect 
+} from "react";
 
 import Columns from "../components/Columns/index";
+import {
+  useAuth0
+} from "../../src/react-auth0-spa";
 
-const Dashboard= () => (
-  <Fragment>
-    <Columns />
-  </Fragment>
-);
+const Dashboard = () => {
+
+  const [showResult, setShowResult] = useState(false);
+  const [apiMessage, setApiMessage] = useState("");
+  const {
+    getTokenSilently, user
+  } = useAuth0();
+
+  const loadUserData = (userToken) => {
+      console.log("Dashboard loading...");
+      getTokenSilently().then((token) => {
+
+        console.log(`User Token - ${token}`);
+        console.log(`Getting notes - ${token}`);
+
+        fetch("/api/notes/get", {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        })
+        .then(response => response.json())
+        .then(data => {
+          console.log('Obtained User Notes:' + data.length);
+          setShowResult(true);
+          setApiMessage(data);
+        });     
+      });  
+  };
+
+  useEffect(() => loadUserData(user.sub), []);
+  
+  return ( 
+    <Fragment>
+       {showResult && <div>{JSON.stringify(apiMessage, null, 2)}</div>}
+      <Columns />
+    </Fragment>
+  )
+
+}
 
 export default Dashboard;
